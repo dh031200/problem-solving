0ab437cfaf5f3ddac1482c7f0300e842b446ed52
dh031200 <dh031200@gmail.com>
2021-06-20 23:59:13 +0900

    etc

---
+++ etc/bj_2490.py
@@ -0,0 +1,39 @@
+a = input().split(' ')
+b = input().split(' ')
+c = input().split(' ')
+
+A = a.count('0')
+if A == 1:
+    print('A')
+elif A == 2:
+    print('B')
+elif A == 3:
+    print('C')
+elif A == 4:
+    print('D')
+elif A == 0:
+    print('E')
+
+B = b.count('0')
+if B == 1:
+    print('A')
+elif B == 2:
+    print('B')
+elif B == 3:
+    print('C')
+elif B == 4:
+    print('D')
+elif B == 0:
+    print('E')
+
+C = c.count('0')
+if C == 1:
+    print('A')
+elif C == 2:
+    print('B')
+elif C == 3:
+    print('C')
+elif C == 4:
+    print('D')
+elif C == 0:
+    print('E')
